054c40bff14f347a88ea4b6b113520217e751c7f
giumas <giuseppemasetti@gmail.com>
2022-03-27 18:44:29 +0200

    bumped version

--- hyo2/bagexplorer/__init__.py
+++ hyo2/bagexplorer/__init__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 name = 'BAG Explorer'
-__version__ = '1.1.0'
-__author__ = 'gmasetti@ccom.unh.edu, brc@ccom.unh.edu'
+__version__ = '1.2.0'
+__author__ = 'gmasetti@ccom.unh.edu'
 __license__ = 'LGPLv3 license'
-__copyright__ = 'Copyright (c) 2021, University of New Hampshire, Center for Coastal and Ocean Mapping'
+__copyright__ = 'Copyright (c) 2022, University of New Hampshire, Center for Coastal and Ocean Mapping'
