003b31131a6178d88ede6c2d4a5bb08576221f4b
giumas <giuseppemasetti@gmail.com>
2022-03-27 20:11:48 +0200

    added hdf_compass namespace to frozen logging

--- freeze/BAGExplorer.py
+++ freeze/BAGExplorer.py
@@ -3,6 +3,6 @@ import logging
 from hyo2.abc.lib.logging import set_logging
 from hyo2.bagexplorer import explorer
 
-set_logging(ns_list=["hyo2.bag", "hyo2.bagexplorer"])
+set_logging(ns_list=["hyo2.bag", "hyo2.bagexplorer", "hdf_compass"])
 
 explorer.run()
